b16fc57ed604e04bbfd170244fbaf6be9aec9348
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-11-04 16:32:41 -0500

    Add content to home section

--- src/home.js
+++ src/home.js
@@ -1,3 +1,24 @@
 export const homeContent = document.createElement("div");
+import chefImage from "./images/chef_photo.jpeg"
 
-homeContent.textContent="Thanks for choosing to dine with us!"
+const introduction = document.createElement("div");
+
+const para1 = document.createElement("p");
+para1.textContent = "Welcome to Fantastic Restaurant, we are very glad that you chose to dine with us!";
+introduction.appendChild(para1);
+
+const para2 = document.createElement("p");
+para2.textContent = "Our main chef Vincent Lemurose is well known for his exquisite and unique creations. Combining flavors from different cultures and using innovative techniques.";
+introduction.appendChild(para2);
+
+const para3 = document.createElement("p");
+para3.textContent = "We aim to provide you the best dining experience with delicious food and excelent service. Have a nice meal!";
+introduction.appendChild(para3);
+
+const photo = document.createElement("img");
+photo.src = chefImage;
+photo.width = "300";
+
+homeContent.classList.add("content");
+homeContent.appendChild(introduction);
+homeContent.appendChild(photo);
