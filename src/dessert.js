e6d19cd487f5aad9d7e60288ed4d6f30296aa9df
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-11-04 17:35:42 -0500

    Add desserts

--- src/dessert.js
+++ src/dessert.js
@@ -1,3 +1,13 @@
 export const dessertContent = document.createElement("div");
+import { menuItem } from "./menuItem";
+import photo1 from "./images/dessert1-pannacotta.png";
+import photo2 from "./images/dessert2-moltenchocolate.png";
 
-dessertContent.textContent="Finish your meal with our finest desserts!"
+const title = document.createElement("h2");
+title.textContent="Finish your meal with our finest desserts!";
+
+dessertContent.appendChild(title);
+
+dessertContent.appendChild(menuItem("Vanilla Bean Panna Cotta with Berry Compote", "A creamy, silky vanilla bean panna cotta, delicately sweetened and topped with a vibrant berry compote made from fresh strawberries, blueberries, and raspberries. Finished with a hint of mint for a refreshing touch.","$8.50", photo1));
+
+dessertContent.appendChild(menuItem("Warm Molten Chocolate Lava Cake","A rich, decadent chocolate cake with a molten center that oozes with warm chocolate ganache upon cutting. Served with a scoop of vanilla bean ice cream and a sprinkle of powdered sugar. A chocolate lover’s dream!", "$9.00", photo2));
