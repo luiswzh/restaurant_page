cca03dac6d26ee34d031dcd0cc27a2d4bcdde453
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-11-04 17:41:46 -0500

    Add drinks section

--- src/drink.js
+++ src/drink.js
@@ -1,3 +1,13 @@
 export const drinkContent = document.createElement("div");
+import { menuItem } from "./menuItem";
+import photo1 from "./images/drink1-lemonade.png";
+import photo2 from "./images/drink2-oldfashioned.png";
 
-drinkContent.textContent="Drinks!"
+const title = document.createElement("h2");
+title.textContent="Drinks!";
+
+drinkContent.appendChild(title);
+
+drinkContent.appendChild(menuItem("Lavender Lemonade Sparkler", "A refreshing, floral lemonade infused with hints of lavender and topped with sparkling water for a bubbly finish. Served over ice with a twist of lemon and a sprig of fresh lavender. Light, aromatic, and perfect for any occasion.","$5.00", photo1));
+
+drinkContent.appendChild(menuItem("Classic Old Fashioned","A timeless cocktail made with premium bourbon, a touch of sugar, aromatic bitters, and a hint of orange peel. Served over a large ice cube and garnished with a cherry and orange zest. Smooth and sophisticated.", "$12.00", photo2));
